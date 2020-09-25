27814afa774809b08afa7ca8ad98039c1361a573
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-09-25 03:43:59 -0400

    Define virtual functions

--- src/orders/Order.cpp
+++ src/orders/Order.cpp
@@ -1,6 +1,25 @@
 #include <Order.hpp>
 
 Order::~Order() {}
+
+bool Deploy::validate() { return true; }
+bool Deploy::execute(Player* player) { return true; }
+
+bool Advance::validate() { return true; }
+bool Advance::execute(Player* player) { return true; }
+
+bool Bomb::validate() { return true; }
+bool Bomb::execute(Player* player) { return true; }
+
+bool Blockade::validate() { return true; }
+bool Blockade::execute(Player* player) { return true; }
+
+bool Airlift::validate() { return true; }
+bool Airlift::execute(Player* player) { return true; }
+
+bool Negotiate::validate() { return true; }
+bool Negotiate::execute(Player* player) { return true; }
+
 OrdersList::OrdersList(vector<Order*>* orders) : orders(*orders) {}
 
 OrdersList::~OrdersList() {
