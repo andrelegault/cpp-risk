313f5ebd4bace6b7b2a2f906da58cb4f0abca4da
alexandra-lelyuk <71044189+alexandra-lelyuk@users.noreply.github.com>
2020-10-07 20:34:36 -0400

    Copy constructor + destructor
    waiting for deep copy constructors from other classes

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -9,9 +9,37 @@ Player::Player() {
 
 }
 
-//TODO look more into destructor
+//TODO wait for ALL OTHER CLASSES used to have their deep copy constructors implemented
+
+Player::Player(const Player& player){
+    this->orders = new OrdersList(player->orders);
+
+    vector<Territory*> newTerList;
+    for(int i = 0; i< this->territories.size(); i++){
+        Territory* newTer = new Territory(this->territories);
+        newTerList.push_back(newTer);
+    }
+    this->territories = newTerList;
+
+    
+    //this->hand = new Hand(player->hand);
+}
+
+//TODO delete hand when implemented
 Player::~Player() {
     cout << "Player is being deleted" << endl;
+    delete orders;
+    orders = NULL;
+
+    for(int i = 0; i< territories.size(); i++){
+        delete territories.at(i);
+        territories.at(i) = NULL;
+    }
+
+    /*
+    delete hand;
+    hand = NULL;
+    */
 }
 
 //TODO get the name of the territory??
