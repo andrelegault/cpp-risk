2680b908c2962542cb35ea2e24a86c8ba7a3537e
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2020-10-18 01:54:06 -0400

    🚩Started of the GameEngine class.

--- src/game/GameEngine.cpp
+++ src/game/GameEngine.cpp
@@ -1,1 +1,70 @@
 #include <GameEngine.hpp>
+
+GameEngine::GameEngine() {
+    UI::clear();
+
+    switch (ask(Banner(), { "Start Game", "Exit" })) {
+    case 2:
+        return;
+    }
+
+    string directory = "maps/";
+
+    vector<string> maps;
+
+    for (const auto& entry : directory_iterator(directory)) {
+        string path = entry.path();
+
+        auto pathSplit = split(path, "/");
+
+        string file = pathSplit[pathSplit.size() - 1];
+
+        auto fileSplit = split(file, ".");
+
+        string mapName = fileSplit[0];
+
+        string fileExtension = fileSplit[fileSplit.size() - 1];
+
+        if (fileExtension == "map") {
+            maps.push_back(mapName);
+        }
+    }
+
+    UI::clear();
+
+    Map mapObj = MapLoader::load(directory + maps[ask("Select Map", maps) - 1] + ".map");
+
+    if (!mapObj.validate()) {
+        UI::clear();
+
+        cout << Grid(new Text("Invalid Map"));
+
+        return;
+    }
+
+    this->map = new Map(mapObj);
+
+    this->deck = new Deck();
+
+    UI::clear();
+
+    int numberOfPlayers = range("Number of Players", 2, 5);
+
+    for (int i = 0; i < numberOfPlayers; i++) {
+        this->players.push_back(new Player(*this->deck));
+    }
+
+    UI::clear();
+
+    bool useObserver = !(ask("Observer?", { "Yes", "No" }) - 1);
+}
+
+void GameEngine::startupPhase() {}
+
+void GameEngine::mainGameLoop() {}
+
+void GameEngine::reinforcementPhase() {}
+
+void GameEngine::issueOrdersPhase() {}
+
+void GameEngine::executeOrdersPhase() {}
--- src/game/GameEngine.hpp
+++ src/game/GameEngine.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <UI.hpp>
+#include <Player.hpp>
+#include <Utils.hpp>
+#include <MapLoader.hpp>
+#include <Map.hpp>
+#include <Cards.hpp>
+#include <filesystem>
+
+using namespace filesystem;
+using namespace UI;
+
+class GameEngine {
+private:
+    vector<Player*> players;
+    Map* map;
+    Deck* deck;
+
+public:
+    GameEngine();
+
+    void startupPhase();
+
+    void mainGameLoop();
+
+    void reinforcementPhase();
+
+    void issueOrdersPhase();
+
+    void executeOrdersPhase();
+};
