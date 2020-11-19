9fa2d89984ecb54f677f757619b7ecf850541a4c
Andrew H <andrew@plankdesign.com>
2020-11-19 00:12:32 -0500

    MapLoader shouldn't expect an empty line at the end of map files + readd eurasian.map

--- src/map/MapLoader.cpp
+++ src/map/MapLoader.cpp
@@ -42,19 +42,16 @@ Map* MapLoader::load(const string file_name) {
 
         // Find related files save to 2d vector for now...
         if (line.find("[files]") == 0) {
-            getline(file, line);
-            while (line.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
-                vector<string> words = Utils::split(Utils::trim(line));
+            while (getline(file, line) && !Utils::trim(line).empty()) {
+                vector<string> words = Utils::split(line);
                 files.push_back(words);
-                getline(file, line);
             }
         }
 
         // Create Continents
         if (line.find("[continents]") == 0) {
-            getline(file, line);
-            while (line.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
-                vector<string> words = Utils::split(Utils::trim(line));
+            while (getline(file, line) && !Utils::trim(line).empty()) {
+                vector<string> words = Utils::split(line);
                 if (!(words.size() == 3 || words.empty())) {
                     throw logic_error("Invalid Continent Format");
                 }
@@ -64,15 +61,13 @@ Map* MapLoader::load(const string file_name) {
                 // 2 - color
 
                 continents.push_back(new Continent(words[0], stoi(words[1])));
-                getline(file, line);
             }
         }
 
         // Create Territories and connect them to their Continents
         if (line.find("[countries]") == 0) {
-            getline(file, line);
-            while (line.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
-                vector<string> words = Utils::split(Utils::trim(line));
+            while (getline(file, line) && !Utils::trim(line).empty()) {
+                vector<string> words = Utils::split(line);
                 if (!(words.size() == 5 || words.empty())) {
                     throw logic_error("Invalid Country Format");
                 }
@@ -88,15 +83,13 @@ Map* MapLoader::load(const string file_name) {
                 continent->connect(country);
 
                 countries.push_back(country);
-                getline(file, line);
             }
         }
 
         // Connect MapNodes with eachother to form Borders between Territories
         if (line.find("[borders]") == 0) {
-            getline(file, line);
-            while (line.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
-                vector<string> words = Utils::split(Utils::trim(line));
+            while (getline(file, line) && !Utils::trim(line).empty()) {
+                vector<string> words = Utils::split(line);
                 if (words.size() < 2) {
                     throw logic_error("Invalid Border Format");
                 }
@@ -109,8 +102,6 @@ Map* MapLoader::load(const string file_name) {
                 for (int i = 1; i < words.size(); i++) {
                     parent->connect(countries[stoi(words[i]) - 1]);
                 }
-
-                getline(file, line);
             }
         }
     }
