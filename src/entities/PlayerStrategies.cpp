d3f6fd7b88cf6d470779018d8de949ee307de9c5
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2020-11-23 19:11:35 -0500

    Added Player to Strat cpp.

--- src/entities/PlayerStrategies.cpp
+++ src/entities/PlayerStrategies.cpp
@@ -4,38 +4,38 @@
  * HUMAN STRATEGY
  *****************************************************/
 
-void HumanPlayerStrategy::issueOrder(){}
+void HumanPlayerStrategy::issueOrder(Player* player){}
 
-vector<Territory*>  HumanPlayerStrategy::toAttack(){}
+vector<Territory*>  HumanPlayerStrategy::toAttack(Player* player){}
 
-vector<Territory*>  HumanPlayerStrategy::toDefend(){}
+vector<Territory*>  HumanPlayerStrategy::toDefend(Player* player){}
 
 /******************************************************
  * AGGRESIVE STRATEGY
  *****************************************************/
 
-void AgressivePlayerStrategy::issueOrder(){}
+void AgressivePlayerStrategy::issueOrder(Player* player){}
 
-vector<Territory*>  AgressivePlayerStrategy::toAttack(){}
+vector<Territory*>  AgressivePlayerStrategy::toAttack(Player* player){}
 
-vector<Territory*>  AgressivePlayerStrategy::toDefend(){}
+vector<Territory*>  AgressivePlayerStrategy::toDefend(Player* player){}
 
 /******************************************************
  * BENEVOLENT STRATEGY
  *****************************************************/
 
-void BenevolentPlayerStrategy::issueOrder(){}
+void BenevolentPlayerStrategy::issueOrder(Player* player){}
 
-vector<Territory*>  BenevolentPlayerStrategy::toAttack(){}
+vector<Territory*>  BenevolentPlayerStrategy::toAttack(Player* player){}
 
-vector<Territory*>  BenevolentPlayerStrategy::toDefend(){}
+vector<Territory*>  BenevolentPlayerStrategy::toDefend(Player* player){}
 
 /******************************************************
  * NEUTRAL PLAYER STRATEGY
  *****************************************************/
 
-void NeutralPlayerStrategy::issueOrder(){}
+void NeutralPlayerStrategy::issueOrder(Player* player){}
 
-vector<Territory*>  NeutralPlayerStrategy::toAttack(){}
+vector<Territory*>  NeutralPlayerStrategy::toAttack(Player* player){}
 
-vector<Territory*>  NeutralPlayerStrategy::toDefend(){}
+vector<Territory*>  NeutralPlayerStrategy::toDefend(Player* player){}
