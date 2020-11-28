2f032e6d038f2ea9925d567dc35908e6bb60634f
Sherif Ghaffar <sherif-99@hotmail.com>
2020-11-28 13:16:48 -0500

    Update MapLoader.hpp

--- src/map/MapLoader.hpp
+++ src/map/MapLoader.hpp
@@ -22,7 +22,7 @@ public:
 
     /**
      * Copy constructor.
-     * @param other Other maploader object.
+     * @param other Other maploader object. 
      */
     MapLoader(const MapLoader& other);
 
@@ -47,5 +47,43 @@ public:
      * @return A Map graph represenation of the Conquest Map file.
      * @throw If the file does not exist or the file is invalid.
      */
-    static Map* load(const string fileName);
+    virtual Map* load(const string fileName);
+};
+
+class ConquestFileReader {
+public:
+    ConquestFileReader();
+    ConquestFileReader(const ConquestFileReader& other);
+
+    /**
+     * Copy constructor.
+     * @param other Other maploader object.
+     */
+    ConquestFileReader(const MapLoader& other);
+
+    /**
+     * Assignment operator overload.
+     * @param other Other object used for assignment.
+     * @return Reference to shallow copy.
+     */
+    ConquestFileReader& operator=(const ConquestFileReader& other);
+
+    /**
+     * Stream insertion operator.
+     * @param stream Stream to output to.
+     * @param maploader Object to output.
+     */
+    friend ostream& operator<<(ostream& stream, const ConquestFileReader& conquestFileReader);
+
+    virtual Map* load(const string fileName);
+};
+
+
+class ConquestFileReaderAdapter: public MapLoader{
+private:
+    ConquestFileReader conquestFileReader;
+public:
+    ConquestFileReaderAdapter(const ConquestFileReader& conquestFile);
+    Map* load(const string fileName);
+
 };
